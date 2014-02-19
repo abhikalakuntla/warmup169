fd6514e946a9e08e6a7963b09cb7c62e707fce08
Abhi Kalakuntla <abhi.kalakuntla@gmail.com>
2014-02-19 04:41:35 -0800

    production variable at the top for what database to connect to

--- warmup169/settings.py
+++ warmup169/settings.py
@@ -66,7 +66,12 @@ WSGI_APPLICATION = 'warmup169.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 if(PRODUCTION == True):
-	
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.sqlite3',
+			'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		}
+	}
 
 	# Parse database configuration from $DATABASE_URL
 	import dj_database_url
