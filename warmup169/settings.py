2cb2284e16e3a11e4855c6f99a329bad1a0f3d25
Abhi Kalakuntla <abhi.kalakuntla@gmail.com>
2014-02-19 03:56:58 -0800

    test for live

--- warmup169/settings.py
+++ warmup169/settings.py
@@ -57,15 +57,15 @@ WSGI_APPLICATION = 'warmup169.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-""""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-"""
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',       # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -75,7 +75,7 @@ DATABASES = {
         'HOST': '',                                 # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                                 # Set to empty string for default. Not used with sqlite3.
     }
-}
+}"""
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
@@ -89,14 +89,15 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-"""
+
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-"""
+
+
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
